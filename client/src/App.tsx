488acab0c8dd7a41a1bc2dcf42b2cbef633a9855
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-11-09 17:03:49 +0800

    lesson 4a: initialize React

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Container, Stack } from "@chakra-ui/react"
+
+
+const App = () => {
+  return (
+    <Stack h="100vh">
+      
+      <Container>
+        
+      </Container>
+    </Stack>
+  );
+}
+
+export default App
